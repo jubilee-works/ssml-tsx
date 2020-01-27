1b9e23f612a15913ee0cd9cac1dc21626f78db67
Yusuke Fujiki <yusuke@fujikkys.com>
2020-01-27 23:59:43 +0900

    ci(semantic-release): add release.config.js

---
+++ release.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+    "@semantic-release/npm",
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ]
+  ]
+};
