3f15f11fdaca280a98c52b95c4d91d4a2d67a09b
Yusuke Fujiki <yusuke@fujikkys.com>
2020-01-28 10:16:16 +0900

    fix(jest): add some tests

--- src/__tests__/renderToString.spec.tsx
+++ src/__tests__/renderToString.spec.tsx
@@ -2,23 +2,37 @@
 import ssml, { renderToString, FC } from "..";
 
 describe("renderToString", () => {
-  test("should match expected output.", () => {
-    const Foo: FC<{ name: string }> = ({ name }) => (
-      <speak>
-        <say-as interpret-as="characters">{name}</say-as>
-        <break time="2s" />
-        <p>What would you like to do today?</p>
-      </speak>
-    );
+  const Foo: FC<{ name: string }> = ({ name }) => (
+    <speak>
+      <say-as interpret-as="characters">{name}</say-as>
+      <break time="2s" />
+      <p>What would you like to do today?</p>
+    </speak>
+  );
 
-    const element1 = <Foo name="foo" />;
-    const element2 = <say-as interpret-as="date">foo</say-as>;
+  const component = <Foo name="foo" />;
+  const jsx = <say-as interpret-as="date">foo</say-as>;
 
-    expect(renderToString(element1)).toBe(
+  test("should match expected output with `renderToString`", () => {
+    expect(renderToString(component)).toBe(
       `<speak><say-as interpret-as="characters">foo</say-as><break time="2s"/><p>What would you like to do today?</p></speak>`
     );
-    expect(renderToString(element2)).toBe(
+    expect(renderToString(jsx)).toBe(
       `<say-as interpret-as="date">foo</say-as>`
     );
+  });
+
+  test("should match expected output with `toString`", () => {
+    expect(component.toString()).toBe(
+      `<speak><say-as interpret-as="characters">foo</say-as><break time="2s"/><p>What would you like to do today?</p></speak>`
+    );
+    expect(jsx.toString()).toBe(`<say-as interpret-as="date">foo</say-as>`);
+  });
+
+  test("should match expected output with template literal", () => {
+    expect(`${component}`).toBe(
+      `<speak><say-as interpret-as="characters">foo</say-as><break time="2s"/><p>What would you like to do today?</p></speak>`
+    );
+    expect(`${jsx}`).toBe(`<say-as interpret-as="date">foo</say-as>`);
   });
 });
